eb329a7955f662241440c4f78d551fef60aa57d4
Miguel Andrada <cp.miguelandrada@gmail.com>
2026-05-27 19:11:48 -0300

    fix webhook syntax

--- app/api/create-preference/webhook/route.ts
+++ app/api/create-preference/webhook/route.ts
@@ -3,59 +3,93 @@ import { supabase } from "@/app/lib/supabase";
 
 export async function POST(req: Request) {
   try {
+
     const body = await req.json();
 
     console.log("WEBHOOK:");
     console.log(body);
 
     if (body.type === "payment") {
+
       const paymentId = body.data.id;
 
-      console.log("PAYMENT ID:", paymentId);
+      console.log(
+        "PAYMENT ID:",
+        paymentId
+      );
 
-      // DEMO:
-      // aprobar última inversión pending
+      const { data: investments } =
+        await supabase
+          .from("investments")
+          .select("*")
+          .eq("status", "pending")
+          .limit(1);
 
-      const { data: investments } await supabase
-  .from("investments")
-  .update({
-    status: "approved",
-  })
-  .eq("id", investment.id);
+      if (
+        investments &&
+        investments.length > 0
+      ) {
 
-const { data: project } =await supabase
-    .from("projects")
-    .select("current_amount")
-    .eq("id", investment.project_id)
-    .single();
+        const investment =
+          investments[0];
 
-if (project) {
+        await supabase
+          .from("investments")
+          .update({
+            status: "approved",
+          })
+          .eq(
+            "id",
+            investment.id
+          );
 
-  await supabase
-    .from("projects")
-    .update({
-      current_amount:
-        Number(project.current_amount || 0) +
-        Number(investment.amount),
-    })
-    .eq(
-      "id",
-      investment.project_id
-    );
+        const {
+          data: project,
+        } = await supabase
+          .from("projects")
+          .select(
+            "current_amount"
+          )
+          .eq(
+            "id",
+            investment.project_id
+          )
+          .single();
 
-};
+        if (project) {
+
+          await supabase
+            .from("projects")
+            .update({
+              current_amount:
+                Number(
+                  project.current_amount || 0
+                ) +
+                Number(
+                  investment.amount
+                ),
+            })
+            .eq(
+              "id",
+              investment.project_id
+            );
+
+        }
 
         console.log(
           "INVERSIÓN APROBADA:",
           investment.id
         );
+
       }
     }
 
     return NextResponse.json({
       success: true,
     });
+
   } catch (error) {
+
     console.log(error);
 
     return NextResponse.json(
